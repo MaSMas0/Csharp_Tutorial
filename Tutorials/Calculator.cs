dccbd2394315bccb803f025aadc06ba6dca7f810
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-15 02:20:28 +0200

    add tutorials

---
+++ Tutorials/Calculator.cs
@@ -0,0 +1,35 @@
+﻿// using System;
+
+//namespace Tutorials
+//{
+//    class MainClass
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter First Number : ");
+//            Double num1= int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter Second Number : ");
+//            Double num2 = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter the operation you want to do (m,d,s,a)");
+//            String answer = Console.ReadLine();
+//            switch(answer)
+//            {
+//                case "m":
+//                    Console.WriteLine($"{num1} multiplied by {num2} = {num1 * num2}");
+//                    break;
+//                case "d":
+//                    Console.WriteLine($"{num1} dvided by {num2} = {num1 / num2}");
+//                    break;
+//                case "s":
+//                    Console.WriteLine($"{num1} subtracted by {num2} = {num1 - num2}");
+//                    break;
+//                case "a":
+//                    Console.WriteLine($"{num1} add to {num2} = {num1 + num2}");
+//                    break;
+
+//            }
+
+
+//        }
+//    }
+//}
---
+++ Tutorials/Madlib_game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tutorials
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string color, pluralNoun, Celebrity;
+
+            Console.WriteLine("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Enter a Celebrity name: ");
+            Celebrity = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"roses are {color}\n{pluralNoun} are blue\nI love {Celebrity}");
+        }
+    }
+}
