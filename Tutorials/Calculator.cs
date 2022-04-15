b660a2c7a5dd20002939989257602b43dd6e5d75
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-15 14:13:11 +0200

    add exception handlers

--- Tutorials/Calculator.cs
+++ Tutorials/Calculator.cs
@@ -6,27 +6,35 @@ namespace Tutorials
     {
          static void Main(string[] args)
         {
-            Console.WriteLine("Enter First Number : ");
-            Double num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Second Number : ");
-            Double num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the operation you want to do (m,d,s,a)");
-            String answer = Console.ReadLine();
-            switch (answer)
+            try
             {
-                case "m":
-                    Console.WriteLine($"{num1} multiplied by {num2} = {num1 * num2}");
-                    break;
-                case "d":
-                    Console.WriteLine($"{num1} dvided by {num2} = {num1 / num2}");
-                    break;
-                case "s":
-                    Console.WriteLine($"{num1} subtracted by {num2} = {num1 - num2}");
-                    break;
-                case "a":
-                    Console.WriteLine($"{num1} add to {num2} = {num1 + num2}");
-                    break;
+                Console.WriteLine("Enter First Number : ");
+                Double num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number : ");
+                Double num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the operation you want to do (m,d,s,a)");
+                String answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "m":
+                        Console.WriteLine($"{num1} multiplied by {num2} = {num1 * num2}");
+                        break;
+                    case "d":
+                        Console.WriteLine($"{num1} dvided by {num2} = {num1 / num2}");
+                        break;
+                    case "s":
+                        Console.WriteLine($"{num1} subtracted by {num2} = {num1 - num2}");
+                        break;
+                    case "a":
+                        Console.WriteLine($"{num1} add to {num2} = {num1 + num2}");
+                        break;
 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine($"Oops you fell into an error :D {e.Message}");
             }
 
 
--- Tutorials/Exponent_Method/Program.cs
+++ Tutorials/Exponent_Method/Program.cs
@@ -6,20 +6,29 @@ namespace Exponent_Method
     {
         static void Main(string[] args)
         {
-            static int GetPow(int baseNum, int powNum)
+            try
             {
-                int result = 1;
-                    for (int i =0; i < powNum; i++)
+                static int GetPow(int baseNum, int powNum)
                 {
-                    result = result * baseNum;
+                    int result = 1;
+                    for (int i = 0; i < powNum; i++)
+                    {
+                        result = result * baseNum;
+                    }
+                    return result;
                 }
-                return result;
+                Console.WriteLine("Enter Base Number: ");
+                int baseNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Power Number: ");
+                int powNum = int.Parse(Console.ReadLine());
+                Console.WriteLine($"the result of {baseNum} powered to {powNum} = {GetPow(baseNum, powNum)}");
             }
-            Console.WriteLine("Enter Base Number: ");
-            int baseNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Power Number: ");
-            int powNum = int.Parse(Console.ReadLine());
-            Console.WriteLine($"the result of {baseNum} powered to {powNum} = {GetPow(baseNum, powNum)}");
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine($"Oops you fell into an error :D {e.Message}");
+            }
+            
         }
     }
 }
