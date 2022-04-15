2d4dc43adcdc737edd3604e23e5305f622bc3f09
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-15 13:47:44 +0200

    add exponent method tutorial

---
+++ Tutorials/Exponent_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exponent_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int GetPow(int baseNum, int powNum)
+            {
+                int result = 1;
+                    for (int i =0; i < powNum; i++)
+                {
+                    result = result * baseNum;
+                }
+                return result;
+            }
+            Console.WriteLine("Enter Base Number: ");
+            int baseNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Power Number: ");
+            int powNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"the result of {baseNum} powered to {powNum} = {GetPow(baseNum, powNum)}");
+        }
+    }
+}
