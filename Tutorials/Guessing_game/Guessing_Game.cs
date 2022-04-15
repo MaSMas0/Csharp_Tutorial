29a53426a23889460674b18e23b3f9571998f40a
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-15 13:23:39 +0200

    Add a guessing game tutorial

---
+++ Tutorials/Guessing_game/Guessing_Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "eren";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            do
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                    Console.WriteLine("You Lost !");
+
+                }
+
+                if (guess == secretWord)
+                {
+                    Console.WriteLine("You Won !");
+                }
+                
+            } while (guess != secretWord && !outOfGuesses);
+
+
+        }
+    }
+}
