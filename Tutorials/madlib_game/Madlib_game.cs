495ccbed7366bb5fc2da216bcb94e9a74729f953
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-15 02:46:18 +0200

    fix working directories

--- Tutorials/madlib_game/Madlib_game.cs
+++ Tutorials/madlib_game/Madlib_game.cs
@@ -4,7 +4,7 @@ namespace Tutorials
 {
     class Program
     {
-        public static void Main(string[] args)
+         static void Main(string[] args)
         {
             string color, pluralNoun, Celebrity;
 
