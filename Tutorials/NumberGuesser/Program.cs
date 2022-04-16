e476a849991279a3e2811b62c8854dc8ab35651c
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-04-16 13:17:38 +0200

    add play again functionality

--- Tutorials/NumberGuesser/Program.cs
+++ Tutorials/NumberGuesser/Program.cs
@@ -27,50 +27,84 @@ namespace NumberGuesser
 
             Console.WriteLine($"Hello {name}, let's play a game...");
 
-            // Set correct number
-
-            //int correctNumber = 7;        that is fixed let's try random
-
-            // Create a new Random object
-            Random random = new Random();
-
-            // assign a number to be randomly choosen from 1 to 10 
-
-            int correctNumber = random.Next(1, 10);
-
-            // init guess var
-            int guess = 0;
-
-            Console.WriteLine("Guess a number between 1 and 10");
-
-            // Do While guess is not correct
-
-            do
+            while (true)
             {
-                string input = Console.ReadLine();
-                if(!int.TryParse(input, out guess))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Please enter an actual number");
-                    Console.ResetColor();
 
-                    // Keep going
+                // Set correct number
+
+                //int correctNumber = 7;        that is fixed let's try random
+
+                // Create a new Random object
+                Random random = new Random();
+
+                // assign a number to be randomly choosen from 1 to 10 
+
+                int correctNumber = random.Next(1, 10);
+
+                // init guess var
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                // Do While guess is not correct
+
+                do
+                {
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter an actual number");
+                        Console.ResetColor();
+
+                        // Keep going
+                        continue;
+                    }
+                    guess = int.Parse(input);
+                    if (guess == correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("You Won!");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong Try again");
+                        Console.ResetColor();
+                    }
+                } while (guess != correctNumber);
+
+                // ask player if he/she wants to play again
+                Console.WriteLine("wanna play again? [Y or N]");
+
+                // Get an answer
+
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "Y")
+                {
                     continue;
                 }
-                guess = int.Parse(input);
-                if (guess == correctNumber)
+                else if (answer == "N")
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine("You Won!");
-                    Console.ResetColor();
+                    return;
                 }
-                else
+                while (answer != "Y" || answer != "N")
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wrong Try again");
-                    Console.ResetColor();
+                    Console.WriteLine("wanna play again? [Y or N]");
+                    answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y")
+                    {
+                        break;
+
+                    }
+                    else if (answer == "N")
+                    {
+                        return;
+                    }
+                    continue;
                 }
-            } while (guess != correctNumber);
+            }
         }
     }
 }
